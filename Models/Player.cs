67ddb10b92846ad1e2e7aa384407e63bcf7271c0
Imran Haidery <iahaidery@gmail.com>
2022-04-26 16:36:19 -0400

    Initial commit

---
+++ Models/Player.cs
@@ -0,0 +1,15 @@
+﻿namespace HVZPlayerTracker.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Status { get; set; }
+
+        public Player()
+        {
+
+        }
+
+    }
+}
